b0e2886d42b4685873f239978afa7f0dd74c0284
Luiz de Lima <luiz12359@gmail.com>
2021-11-12 17:19:38 -0300

    sistema de cadastro em react

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    <span>desenvolvido com <i className="fa fa-heart text-danger"></i> por
+     <strong>Luiz de Lima</strong></span>
+</footer>
